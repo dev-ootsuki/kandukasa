88da74798bc4af0e899cccddc8194ae1dc9721b3
dev-ootsuki <masahiko.ootsuki.1@gmail.com>
2024-12-03 12:40:47 +0900

    UI Helper機能を幾つかに分割

--- frontend/utils/UiHelper.ts
+++ frontend/utils/UiHelper.ts
@@ -1,5 +1,79 @@
 import type { Design } from '~/types/Types'
+import * as DomainClass from '~/types/Domain.class'
 import { useSystemStore } from '~/stores/SystemStore'
+
+/**
+ * 操作全般
+ * mode: 操作モード
+ * label: 画面表示上のラベル
+ * icon: ボタンなどに配置するラベルのmaterial design名称
+ * dangar: 危険な操作か
+ */
+const SystemOperations:Design.SystemOperation[] = [{
+        mode: "register",
+        label: 'common.new_registration_exec',
+        icon: "add",
+        danger:false
+    },{
+        mode: "update",
+        label: 'common.update_exec',
+        icon: "edit",
+        danger:false
+    },{
+        mode:"delete",
+        label: "common.delete_exec",
+        icon: "delete_forever",
+        danger:true
+    },{
+        mode:"truncate",
+        label:"common.truncate_exec",
+        icon:"edit_off",
+        danger:true
+    },{
+        mode:"bulk_delete",
+        label:"common.bulk_delete_exec",
+        icon:"delete_forever",
+        danger:true
+    },{
+        mode:"bulk_truncate",
+        label:"common.bulk_truncate_exec",
+        icon:"edit_off",
+        danger:true
+}]
+
+/**
+ * スキーマの下にある機能まとめ
+ */
+const SchemaSummaries = {
+    // なし
+    "none" :  {label:'',icon:'',show:false, mode:"none"},
+    // トリガーのみ
+    "triggers": {label: 'leftmenu.triggers', icon: "local_fire_department", show:true, "mode": "triggers"},
+    // テーブルのみ
+    "tables" : {label:'leftmenu.tables', icon:"newspaper", show:true, "mode":"tables"},
+    // ビューのみ
+    "views" : {label:'leftmenu.views', icon:"perm_media", show:true, "mode":"views"},
+    // ルーチンのみ
+    "routines": {label:'leftmenu.routines', icon:"functions",show:true, "mode":"routines"},
+    // イベントのみ
+    "events":{label:'leftmenu.events', icon:"calendar_month", show:true, "mode":"events"},
+    // 全部 = スキーマ選択時そのもの
+    "all":{label:'leftmenu.all',icon:"summarize",show:true, "mode":"all"}
+} as {[K:string]:Design.SummaryType}
+
+/**
+ * 各種定義（スキーマ・テーブル詳細・テーブル一覧など）で無視するプロパティ
+ */
+const IgnoreConvertProps = ["id", "schema_id", "table_id", "column_id", "trigger_id", "event_id", "view_id", "routine_id", "events", "views", "triggers", "tables", "routines"]
+/**
+ * テーブル一覧を並べる時の表示順序 = indexkey
+ */
+const TablesOrderDifinitions = ["id", "table_name", "auto_increment", "table_rows", "data_length", "index_length", "avg_row_length", "max_data_length", "engine", "table_collation", "check_time", "table_comment", "table_catalog", "table_type", "create_options", "row_format", "data_free", "checksum", "update_time", "version"]
+/**
+ * テーブル詳細を並べる時の表示順序 = indexkey
+ */
+const ColumnsOrderDifinitions = ["column_name", "ordinal_position", "data_type", "is_nullable", "column_default", "character_maximum_length", "character_octet_length", "numeric_precision", "numeric_scale", "datetime_precision", "character_set_name", "collation_name", "column_type", "column_key", "extra", "privileges", "column_comment", "generation_expression", "srs_id"]
+
 export class UiHelper{
     /**
      * Get current instance's ref ID
@@ -8,29 +82,6 @@ export class UiHelper{
     static getRefId() : string{
         const _ref = getCurrentInstance()?.vnode?.ref as any
         return _ref?.r?.split(".")[1]
-    }
-
-    /**
-     * convert object[] to Quasar Tree (QTree) Node
-     * @param elems Object[]
-     * @param config id set. finalize ids.join(".")
-     * @returns QNode
-     */
-    static createNode(elems: any[], config:{keys:string[], label: string, header:Design.DatabaseTreeNodeHeaderType, expandable?:boolean, selectable?:boolean, lazy?:boolean}) : any[]{
-        if(elems == null)
-            return []
-        const nodes = elems?.map(e => {
-            return {
-                key: config.keys.map(key => { return e[key] !== undefined ? e[key] : key }).join("."),
-                header: config.header,
-                label: e[config.label],
-                children:null,
-                expandable: config.expandable !== undefined ? config.expandable : true,
-                selectable: config.selectable !== undefined ? config.selectable : true,
-                lazy: config.lazy !== undefined ? config.lazy : true,
-            }
-        })
-        return nodes
     }
 
     /**
@@ -57,10 +108,110 @@ export class UiHelper{
         return (id as unknown as Array<string>).map(e => { return parseInt(e) })
     }
 
+    static getSchemaSummary(mode: string) : Design.SummaryType {
+        return SchemaSummaries[mode]
+    }
+
+    static findSystemOperaion(mode:Design.UIMode) : Design.SystemOperation{
+        const ret = SystemOperations.find(e => e.mode == mode)
+        if(ret != null)
+            return ret
+        return SystemOperations[0]
+    }
+}
+
+export class TreeHelper{
+    /**
+     * convert object[] to Quasar Tree (QTree) Node
+     * @param elems Object[]
+     * @param config id set. finalize ids.join(".")
+     * @returns QNode
+     */
+    static createNode(elems: any[], config:{keys:string[], label: string, header:Design.DatabaseTreeNodeHeaderType, expandable?:boolean, selectable?:boolean, lazy?:boolean}) : any[]{
+        if(elems == null)
+            return []
+        const nodes = elems?.map(e => {
+            return {
+                key: config.keys.map(key => { return e[key] !== undefined ? e[key] : key }).join("."),
+                header: config.header,
+                label: e[config.label],
+                children:null,
+                expandable: config.expandable !== undefined ? config.expandable : true,
+                selectable: config.selectable !== undefined ? config.selectable : true,
+                lazy: config.lazy !== undefined ? config.lazy : true,
+            }
+        })
+        return nodes
+    }
+    static createSchemaChildren(t:Function, key:string, data:DomainClass.DbSchema) : any[] {
+        const tables = {
+            label: t('leftmenu.tables'),
+            key:`${key}.tables`,
+            header:"tablesummary",
+            children:TreeHelper.createNode(data.tables, {
+                keys:["id", "schema_id", "tables", "table_name"],
+                label:"table_name",
+                header:"table",
+                lazy:false
+            }),
+            lazy:false
+        }
+        const views = {
+            label:t('leftmenu.views'),
+            key:`${key}.views`,
+            header:"viewsummary",
+            children:TreeHelper.createNode(data.views, {
+                keys:["id", "schema_id", "views", "table_name"],
+                label:"table_name",
+                header:"view",
+                lazy:false
+            }),
+            lazy:false
+        }
+        const triggers = {
+            label:t('leftmenu.triggers'),
+            key:`${key}.triggers`,
+            header:"triggersummary",
+            children:TreeHelper.createNode(data.triggers, {
+                keys:["id", "schema_id", "triggers", "trigger_name"],
+                label:"trigger_name",
+                header:"trigger",
+                lazy:false
+            }),
+            lazy:false
+        }
+        const events = {
+            label:t('leftmenu.events'),
+            key:`${key}.events`,
+            header:"eventsummary",
+            children:TreeHelper.createNode(data.events, {
+                keys:["id", "schema_id", "events", "event_name"],
+                label:"event_name",
+                header:"event",
+                lazy:false
+            }),
+            lazy:false
+        }
+        const routines = {
+            label:t('leftmenu.routines'),
+            key:`${key}.routines`,
+            header:"routinesummary",
+            children:TreeHelper.createNode(data.routines, {
+                keys:["id", "schema_id", "routines", "routine_name"],
+                label:"routine_name",
+                header:"routine",
+                lazy:false
+            }),
+            lazy:false
+        }
+        return [tables, views, triggers, routines, events]
+    }
+}
+
+export class TableHelper{
     /**
      * ignore [ information of spec to this apps ]
      */
-    private static ignoreConvertProps = ["id", "schema_id", "table_id", "column_id", "trigger_id", "event_id", "view_id", "routine_id", "events", "views", "triggers", "tables", "routines"]
     /**
      * convert object to QTable Column
      * @param elem results of query
@@ -70,7 +221,7 @@ export class UiHelper{
         if(elem === undefined)
             return []
         return Object.getOwnPropertyNames(elem).map(e => {
-            if(this.ignoreConvertProps.includes(e))
+            if(IgnoreConvertProps.includes(e))
                 return undefined
             return {
                 name: e,
@@ -82,9 +233,8 @@ export class UiHelper{
             }
         }).filter(e => e)
     }
-    private static tablesOrderDifinitions = ["id", "table_name", "auto_increment", "table_rows", "data_length", "index_length", "avg_row_length", "max_data_length", "engine", "table_collation", "check_time", "table_comment", "table_catalog", "table_type", "create_options", "row_format", "data_free", "checksum", "update_time", "version"]
     static createTableColumn($t: Function) : any[]{
-        return this.tablesOrderDifinitions.map(each => {
+        return TablesOrderDifinitions.map(each => {
             return {
                 name: each,
                 required: false,
@@ -95,9 +245,8 @@ export class UiHelper{
             }
         })
     }
-    private static  columnsOrderDifinitions = ["column_name", "ordinal_position", "data_type", "is_nullable", "column_default", "character_maximum_length", "character_octet_length", "numeric_precision", "numeric_scale", "datetime_precision", "character_set_name", "collation_name", "column_type", "column_key", "extra", "privileges", "column_comment", "generation_expression", "srs_id"]
     static createColumns($t: Function) : any[]{
-        return this.columnsOrderDifinitions.map(each => {
+        return ColumnsOrderDifinitions.map(each => {
             return {
                 name: each,
                 required: true,
@@ -132,63 +281,4 @@ export class UiHelper{
             data_type: "system_primary"
         }].concat(ret)
     }
-
-    /**
-     * Leftmenu Depth 3 = table or trigger or views or events or routines
-     * this is provided functions to detemine and process these 
-     */
-    private static SchemaSummaries = {
-        "none" :  {label:'',icon:'',show:false, mode:"none"},
-        "triggers": {label: 'leftmenu.triggers', icon: "local_fire_department", show:true, "mode": "triggers"},
-        "tables" : {label:'leftmenu.tables', icon:"newspaper", show:true, "mode":"tables"},
-        "views" : {label:'leftmenu.views', icon:"perm_media", show:true, "mode":"views"},
-        "routines": {label:'leftmenu.routines', icon:"functions",show:true, "mode":"routines"},
-        "events":{label:'leftmenu.events', icon:"calendar_month", show:true, "mode":"events"},
-        "all":{label:'leftmenu.all',icon:"summarize",show:true, "mode":"all"}
-    } as {[K:string]:Design.SummaryType}
-
-    static getSchemaSummary(mode: string) : Design.SummaryType {
-        return this.SchemaSummaries[mode]
-    }
-
-    private static SystemOperations:Design.SystemOperation[] = [{
-            mode: "register",
-            label: 'common.new_registration_exec',
-            icon: "add",
-            danger:false
-        },{
-            mode: "update",
-            label: 'common.update_exec',
-            icon: "edit",
-            danger:false
-        },{
-            mode:"delete",
-            label: "common.delete_exec",
-            icon: "delete_forever",
-            danger:true
-        },{
-            mode:"truncate",
-            label:"common.truncate_exec",
-            icon:"edit_off",
-            danger:true
-        },{
-            mode:"bulk_delete",
-            label:"common.bulk_delete_exec",
-            icon:"delete_forever",
-            danger:true
-        },{
-            mode:"bulk_truncate",
-            label:"common.bulk_truncate_exec",
-            icon:"edit_off",
-            danger:true
-        }
-    ]
-    static findSystemOperaion(mode:Design.UIMode) : Design.SystemOperation{
-        const ret = this.SystemOperations.find(e => e.mode == mode)
-        if(ret != null)
-            return ret
-        return this.SystemOperations[0]
-    }
 }
-
-
